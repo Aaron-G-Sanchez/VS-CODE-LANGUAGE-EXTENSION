c32cad636931925d0fa0399869339348ba94680f
Aaron Sanchez <saarong12@gmail.com>
2024-04-18 00:40:47 -0500

    Added activation event and can now log when changes are made to the focused file in the workspace

--- extension.js
+++ extension.js
@@ -1,37 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "todo-detection" is now active!')
-  console.log('Hello World')
+  // Command to show activation
+  console.log('Activated')
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'todo-detection.helloWorld',
-    function () {
-      // The code you place here will be executed every time your command is executed
+  // let todoTracker = []
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from Todo-detection!')
-    }
-  )
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    console.log(e)
 
-  context.subscriptions.push(disposable)
+    console.log(e.document.getWordRangeAtPosition())
+    // todoTracker.push(e.contentChanges[0].text)
+
+    // console.log(todoTracker.join(''))
+  })
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+  console.log('Deactivated')
+}
 
 module.exports = {
   activate,
